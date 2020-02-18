2cbbf4bf4a96116090bbdd290c865b071bd911b3
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-02-17 19:08:31 -0800

    Add project files.

---
+++ UWPSoundBoard/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPSoundBoard.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPSoundBoard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Sound> sounds;
+        private List<MenuItem> menuItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            sounds = new ObservableCollection<Sound>();
+            SoundManager.GetAllSounds(sounds);
+
+            menuItems = new List<MenuItem>();
+            //Load pane
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/animals.png",
+                Category = SoundCategory.Animals
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/cartoon.png",
+                Category = SoundCategory.Cartoons
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/taunt.png",
+                Category = SoundCategory.Taunts
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/warning.png",
+                Category = SoundCategory.Warnings
+            });
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundManager.GetAllSounds(sounds);
+            CategoryTextBlock.Text = "All Sounds";
+            MenuItemsListView.SelectedItem = null;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            SoundManager.GetSoundsByCategory(sounds, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var sound = (Sound)e.ClickedItem;
+            MyMediaElement.Source = new Uri(this.BaseUri, sound.AudioFile);
+        }
+    }
+}
---
+++ UWPSoundBoard/Model/Sound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+    public class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+        public Sound(string name, SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = $"/Assets/Audio/{category}/{name}.wav";
+            ImageFile = $"/Assets/Images/{category}/{name}.png";
+        }
+    }
+}
